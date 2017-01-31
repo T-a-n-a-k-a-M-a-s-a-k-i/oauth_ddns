6fd1e6f4321ea0c6eff4a3ef0f2b12ab46156b92
Masaki TANAKA <marginal.vivid.worker@gmail.com>
2017-01-31 19:49:46 +0900

    Add Sequel Status model

---
+++ models/status.rb
@@ -0,0 +1,17 @@
+class Status < Sequel::Model
+  plugin :validation_helpers
+  plugin :schema
+  unrestrict_primary_key
+
+  unless table_exists?
+    set_schema do
+      foreign_key :provider, :key=>:provider, :table=>:providers, :on_delete=>:cascade, :type=>String
+      foreign_key :record_type, :key=>:record_type, :table=>:record_types, :on_delete=>:cascade, :type=>String
+      String :user_id
+
+      primary_key([:provider, :record_type, :user_id])
+    end
+
+    create_table
+  end
+end
