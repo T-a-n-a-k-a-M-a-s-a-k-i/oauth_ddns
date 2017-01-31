f291dfc6690a8b77fb1ec5bf37c448b693514cc1
Masaki TANAKA <marginal.vivid.worker@gmail.com>
2017-01-31 20:00:37 +0900

    Add Record Type Model

---
+++ models/record_type.rb
@@ -0,0 +1,13 @@
+class RecordType < Sequel::Model
+  plugin :validation_helpers
+  plugin :schema
+  unrestrict_primary_key
+
+  unless table_exists?
+    set_schema do
+      String :record_type, :primary_key => true
+    end
+
+    create_table
+  end
+end
