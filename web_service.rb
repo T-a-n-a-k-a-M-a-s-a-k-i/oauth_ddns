d307f03ff8bdc96f4e7a9a035ea93ad5a3c9f812
Masaki TANAKA <marginal.vivid.worker@gmail.com>
2017-02-06 03:27:26 +0900

    Add set_status helper method

--- web_service.rb
+++ web_service.rb
@@ -1,6 +1,19 @@
 class WebService < Sinatra::Base
   configure do
     set :sessions, true
+    enable :method_override
+  end
+
+  helpers do
+    def set_status(osn_identifier)
+      @ipv4_status = Status.filter(
+        osn_identifier.merge(:record_type => "ipv4_address")
+      ).first
+
+      @txt_status = Status.filter(
+        osn_identifier.merge(:record_type => "txt")
+      ).first
+    end
   end
 
   use OmniAuth::Builder do
@@ -12,14 +25,30 @@ class WebService < Sinatra::Base
   end
 
   get "/status" do
+    @request = request
+    @ipv4_status
+    @txt_status
+
+    haml :status
+  end
+
+  delete "/status" do
     haml :status
   end
 
   get "/auth/:provider/callback" do
-    auth_result = request.env["omniauth.auth"]
+    omniauth_result = request.env["omniauth.auth"]
+    set_status(
+      :provide => omniauth_result["provider"],
+      :user_id => omniauth_result["info"]["nickname"]
+    )
   end
 
   put "/api/status" do
+
+  end
+
+  delete "/api/status" do
 
   end
 
