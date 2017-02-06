3f376366c2af2c775faf86e9f33e56dad9bbe9f5
alice <marginal.vivid.worker@gmail.com>
2017-02-06 17:52:02 +0900

    Add RubyDNS match handler

--- dns_service.rb
+++ dns_service.rb
@@ -2,15 +2,34 @@ class DNSService
   Name = Resolv::DNS::Name
   IN = Resolv::DNS::Resource::IN
 
+  def self.get_status_by_dns_query(transaction, record_type)
+    status = Status.filter(
+      :user_id => transaction.name.split(".").at(0),
+      :provider => transaction.name.split(".").at(1),
+      :record_type => record_type
+    ).first
+  end
+
   def self.run!
     RubyDNS::run_server(:listen => [[:udp, "0.0.0.0", 53],[:tcp, "0.0.0.0", 53]]) do
       match(/.*/, IN::A) do |transaction|
-      end
+        ipv4_status = self.get_status_by_dns_query(transaction, "ipv4_address")
 
-      match(/.*/, IN::AAAA) do |transaction|
+        if ipv4_status
+          transaction.respond!(ipv4_status.record, resource_class: IN::A, ttl: 1)
+        else
+          transaction.fail!(:NXDomain)
+        end
       end
 
       match(/.*/, IN::TXT) do |transaction|
+        txt_status = self.get_status_by_dns_query(transaction, "txt")
+
+        if txt_status
+          transaction.respond!(txt_status.record, resource_class: IN::TXT, ttl: 1)
+        else
+          transaction.fail!(:NXDomain)
+        end
       end
 
       otherwise do |transaction|
